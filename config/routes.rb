e6e36f7c869b49231bd27f672d26a1231b98aec6
Yudai Watanabe <lock0308@gmail.com>
2018-01-09 17:57:08 +0900

    グループ作成、編集のビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "messages#index"
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :reate, :edit, :update] do
-    resources :messages. only: [:index, :create]
+    resources :messages, only: [:index, :create]
   end
 end
 
