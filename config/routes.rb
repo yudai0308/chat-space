df750e9e1ee3c8ea7589858ae92a602f9e4381b7
Yudai Watanabe <lock0308@gmail.com>
2018-01-09 17:25:49 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :reate, :edit, :update] do
+    resources :messages. only: [:index, :create]
+  end
 end
+
+# Ｑ. ユーザーとメッセージも１対多の関係のはずなのになんでネストさせないの？
+# Ａ. １対多の関係であっても必ずしもネストさせる必要はないから。
+
+# ネストで主に関わってくるのはURLの部分。
+# chat spaceでは「あるグループのメッセージ一覧ページ」が存在する（トップページ）。
+# この時のURLは "chat_groups/19462/messages" などとなり、「グループID/messeges」となっている。
+# グループとメッセージをネストさせているため、このようなURLに設定することができる。
+
+# 逆に、chat spaceには「あるユーザーのメッセージ一覧ページ」は存在しない。
+# そのため、ユーザーとメッセージをネストさせる必要がない。
