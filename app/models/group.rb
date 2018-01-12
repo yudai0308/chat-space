5c8a08b98769d578d992146e8cde2a671f84ceef
Yudai Watanabe <lock0308@gmail.com>
2018-01-12 14:22:49 +0900

    サイドバーに最新メッセージを表示する

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,12 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
 
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : "画像が投稿されました"
+    else
+      "まだメッセージはありません"
+    end
+  end
 end
